7964e945f4f9b44a0b4b4a1a8e0756087a06a6ed
shaxriyor0428 <shaxriyorziyodullayev816@gmail.com>
2024-09-30 13:14:32 +0500

    stadium_finder project created

---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { ComfortModule } from "./comfort/comfort.module";
+import { RegionModule } from "./region/region.module";
+import { DistrictModule } from "./district/district.module";
+import { CategoriesModule } from "./categories/categories.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env",
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_DB,
+      models: [],
+      sync: { alter: true },
+      autoLoadModels: true,
+      synchronize: true,
+      logging: false,
+    }),
+    ComfortModule,
+    RegionModule,
+    DistrictModule,
+    CategoriesModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+async function start() {
+  try {
+    const PORT = process.env.PORT || 3030;
+    const app = await NestFactory.create(AppModule);
+    app.use(cookieParser());
+    await app.listen(PORT, () => {
+      console.log(`Server running at port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+start();
